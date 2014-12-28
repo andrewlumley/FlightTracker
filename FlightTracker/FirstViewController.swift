d6f7f09594c67709fd4c80b22467e4e9aec27bad
Andrew Lumley <andrewlumley@Andrews-MacBook-Pro.local>
2014-12-28 07:04:15 -0500

    Initial Commit

---
+++ FlightTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlightTracker
+//
+//  Created by Andrew Lumley on 2014-12-28.
+//  Copyright (c) 2014 Andrew Lumley. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
